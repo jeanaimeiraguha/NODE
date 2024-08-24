b061742929e904f15cff7a73ea20ecc91dbb7440
jeaniameiraguha <jeaniameiraguha@gmail.com>
2024-08-24 11:04:07 +0200

    mongo

--- MONGO DB/insert.js
+++ MONGO DB/insert.js
@@ -1,3 +1,5 @@
+
+const promise =require('Promise')
 const mongodb = require('mongodb');
 const url='mongodb://localhost://EmployeeDB';
 MongoClient.connect(url,(err,db)=>{
@@ -7,4 +9,17 @@ db.collection('Employee').insertOne({
     EmployeeName:"Iraguha"
 });
 
+//use of promises
+.then(function(db) {
+    db.collection('Employee').insertOne({
+    Employeeid: 4,
+    EmployeeName: "NewEmployee"
+    })
+    .then(function(db1) {
+    db1.collection('Employee').insertOne({
+    Employeeid: 5,
+    EmployeeName: "NewEmployee1"
+    })
+    })
+   });
 });
